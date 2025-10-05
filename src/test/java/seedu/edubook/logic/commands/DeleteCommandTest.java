b903a4c3fc106fffe55d53aa77fb1f59c3798057
Annamalai Maniveerappan <121749563+sabersmash1412@users.noreply.github.com>
2025-10-05 17:23:35 +0800

    Add more tests for DeleteCommand

--- src/test/java/seedu/edubook/logic/commands/DeleteCommandTest.java
+++ src/test/java/seedu/edubook/logic/commands/DeleteCommandTest.java
@@ -6,9 +6,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.edubook.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.edubook.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.edubook.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.edubook.logic.commands.CommandTestUtil.showPersonAtName;
 import static seedu.edubook.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.edubook.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.edubook.testutil.TypicalNames.NAME_FIRST_PERSON;
+import static seedu.edubook.testutil.TypicalNames.NAME_SECOND_PERSON;
 import static seedu.edubook.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
@@ -18,6 +20,7 @@ import seedu.edubook.logic.Messages;
 import seedu.edubook.model.Model;
 import seedu.edubook.model.ModelManager;
 import seedu.edubook.model.UserPrefs;
+import seedu.edubook.model.person.Name;
 import seedu.edubook.model.person.Person;
 
 /**
@@ -71,6 +74,14 @@ public class DeleteCommandTest {
     }
 
     @Test
+    public void execute_invalidNameUnfilteredList_throwsCommandException() {
+        Name invalidName = new Name("John Doe");
+        DeleteCommand deleteCommand = new DeleteCommand(invalidName);
+
+        assertCommandFailure(deleteCommand, model, Messages.MESSAGE_INVALID_NAME);
+    }
+
+    @Test
     public void execute_validIndexFilteredList_success() {
         showPersonAtIndex(model, INDEX_FIRST_PERSON);
 
@@ -101,25 +112,70 @@ public class DeleteCommandTest {
     }
 
     @Test
+    public void execute_validNameFilteredList_success() {
+        showPersonAtName(model, NAME_FIRST_PERSON);
+
+        //Initialise to first person
+        Person personToDelete = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+        for (Person p : model.getFilteredPersonList()) {
+            if (p.getName().equals(NAME_FIRST_PERSON)) {
+                personToDelete = p;
+            }
+        }
+        DeleteCommand deleteCommand = new DeleteCommand(NAME_FIRST_PERSON);
+
+        String expectedMessage = String.format(DeleteCommand.MESSAGE_DELETE_PERSON_SUCCESS,
+                Messages.format(personToDelete));
+
+        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.deletePerson(personToDelete);
+        showNoPerson(expectedModel);
+
+        assertCommandSuccess(deleteCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidNameFilteredList_throwsCommandException() {
+        showPersonAtName(model, NAME_FIRST_PERSON);
+
+        Name invalidName = NAME_SECOND_PERSON;
+        DeleteCommand deleteCommand = new DeleteCommand(invalidName);
+
+        assertCommandFailure(deleteCommand, model, Messages.MESSAGE_INVALID_NAME);
+    }
+
+    @Test
     public void equals() {
         DeleteCommand deleteFirstCommand = new DeleteCommand(INDEX_FIRST_PERSON);
         DeleteCommand deleteSecondCommand = new DeleteCommand(INDEX_SECOND_PERSON);
+        DeleteCommand deleteThirdCommand = new DeleteCommand(NAME_FIRST_PERSON);
+        DeleteCommand deleteFourthCommand = new DeleteCommand(NAME_SECOND_PERSON);
 
         // same object -> returns true
         assertTrue(deleteFirstCommand.equals(deleteFirstCommand));
+        assertTrue(deleteThirdCommand.equals(deleteThirdCommand));
 
         // same values -> returns true
         DeleteCommand deleteFirstCommandCopy = new DeleteCommand(INDEX_FIRST_PERSON);
         assertTrue(deleteFirstCommand.equals(deleteFirstCommandCopy));
 
+        DeleteCommand deleteThirdCommandCopy = new DeleteCommand(NAME_FIRST_PERSON);
+        assertTrue(deleteThirdCommand.equals(deleteThirdCommandCopy));
+
         // different types -> returns false
         assertFalse(deleteFirstCommand.equals(1));
+        assertFalse(deleteThirdCommand.equals(1));
 
         // null -> returns false
         assertFalse(deleteFirstCommand.equals(null));
+        assertFalse(deleteThirdCommand.equals(null));
 
         // different person -> returns false
         assertFalse(deleteFirstCommand.equals(deleteSecondCommand));
+        assertFalse(deleteThirdCommand.equals(deleteFourthCommand));
+
+        // different types of delete -> returns false
+        assertFalse(deleteFirstCommand.equals(deleteThirdCommand));
     }
 
     @Test
