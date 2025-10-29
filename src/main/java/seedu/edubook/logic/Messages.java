280bce6535feed65fa8b9f23f72e244dba28ba98
williamoose <williamjliow@gmail.com>
2025-10-29 15:24:53 +0800

    Change persons to students

--- src/main/java/seedu/edubook/logic/Messages.java
+++ src/main/java/seedu/edubook/logic/Messages.java
@@ -14,9 +14,8 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_NAME = "The person name provided does not exist";
-    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
-    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
+    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The index provided is invalid";
+    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d students listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
                 "Multiple values specified for the following single-valued field(s): ";
     public static final String MESSAGE_CONFLICTING_PREFIXES = "Specify only n/NAME or c/CLASS, not both.";
