40f140d9c5336a1d1015cc1e20398b6f256e1436
Ronnie Fun <ronniefunn@gmail.com>
2025-11-03 20:15:59 +0800

    Remove unnecessary print lines

--- src/test/java/seedu/edubook/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/edubook/logic/parser/AddressBookParserTest.java
@@ -98,8 +98,6 @@ public class AddressBookParserTest {
         EditPersonDescriptor descriptor = new EditPersonDescriptorBuilder(person).build();
         EditCommand command = (EditCommand) parser.parseCommand(EditCommand.COMMAND_WORD + " "
                 + INDEX_FIRST_PERSON.getOneBased() + " " + PersonUtil.getEditPersonDescriptorDetails(descriptor));
-        System.out.println(new EditCommand(INDEX_FIRST_PERSON, descriptor));
-        System.out.println(command);
         assertEquals(new EditCommand(INDEX_FIRST_PERSON, descriptor), command);
     }
 
